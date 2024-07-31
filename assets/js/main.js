e0e437a8f5699b585874ae8c798502b6c6b362a6
Gaurav Saxena <saxenagau@gmail.com>
2024-07-31 18:24:26 +0530

    Updated profile section.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,6 @@
 /**
 * Template Name: iPortfolio
-* Template URL: https://bootstrapmade.com/iportfolio-bootstrap-portfolio-websites-template/
-* Updated: Jun 29 2024 with Bootstrap v5.3.3
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
+* Author: Gaurav Saxena
 */
 
 (function() {
